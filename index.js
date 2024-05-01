22ac45b5dc712c5dd088b3e6107f84e41d190df0
James Chandler <Chandler.790j@gmail.com>
2024-05-01 18:29:59 -0400

    finished questions

--- index.js
+++ index.js
@@ -1,7 +1,58 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "github",
+        message: "Please Enter Your Github username",
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "Please Enter Your Readme Title",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please Enter Your Readme Description",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please Enter Your Installation Process",
+    },
+    {
+        type: "input",
+        name: "usage information",
+        message: "Please Enter Your Usage Information",
+    },
+    {
+        type: "input",
+        name: "contribution guidelines",
+        message: "Please Enter Contribution Guidlines",
+    },
+    {
+        type: "input",
+        name: "test instructions",
+        message: "Please Enter Test Instructions",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please Enter Your Github Email",
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "Please Enter The License You Chose",
+        choices: ["MIT","BSD 3","GPL 3", "ISC","Apache 2.0","Unlicense","N/A"],
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
